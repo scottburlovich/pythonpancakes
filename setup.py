d4256da827ef32f24167bf52fdf9315e279722fd
Scott Burlovich <2328416+scottburlovich@users.noreply.github.com>
2021-07-28 18:32:45 -0400

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="pythonpancakes",
-    version="1.0.0",
+    version="1.0.1",
     description="A basic request wrapper for the PancakeSwap API",
     long_description=README,
     long_description_content_type="text/markdown",

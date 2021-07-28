cb8b801a296989e8d8b634a4d2fc0d619c4700ae
Scott Burlovich <2328416+scottburlovich@users.noreply.github.com>
2021-07-28 16:29:58 -0400

    Use requests.Session() as a context manager, simplify __init__, eliminate __exit__

--- src/pythonpancakes/api.py
+++ src/pythonpancakes/api.py
@@ -2,48 +2,41 @@ import json
 import re
 import requests
 
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-
 
 class PancakeSwapAPI:
     __BASE_URL = "https://api.pancakeswap.info/api/v2/"
+
     """
     Basic API request wrapper for PancakeSwap
     Scott Burlovich (github.com/scottburlovich)
-    Last Update: May 9, 2021
+    Last Update: Jul 28, 2021
     """
 
     def __init__(self, base_url=__BASE_URL):
         self.base_url = base_url
         self.request_timeout = 60
-        self.session = requests.session()
-        retries = Retry(total=3, backoff_factor=0.5, status_forcelist=[502, 503, 504])
-        self.session.mount('https://', HTTPAdapter(max_retries=retries))
-
-    def __exit__(self):
-        self.session.close()
 
     def __get(self, request_url: str):
         """
         GET request wrapper
         :param request_url: str
         """
-        try:
-            response = self.session.get(request_url, timeout=self.request_timeout)
-        except requests.exceptions.RequestException:
-            raise
-
-        try:
-            response.raise_for_status()
-            return json.loads(response.content.decode('utf-8'))
-        except Exception as err:
+        with requests.Session() as session:
             try:
-                content = json.loads(response.content.decode('utf-8'))
-                raise ValueError(content)
-            except json.decoder.JSONDecodeError:
-                pass
-            raise
+                response = session.get(request_url, timeout=self.request_timeout)
+            except requests.exceptions.RequestException:
+                raise
+
+            try:
+                response.raise_for_status()
+                return json.loads(response.content.decode('utf-8'))
+            except Exception as err:
+                try:
+                    content = json.loads(response.content.decode('utf-8'))
+                    raise ValueError(content)
+                except json.decoder.JSONDecodeError:
+                    pass
+                raise
 
     def summary(self):
         """
